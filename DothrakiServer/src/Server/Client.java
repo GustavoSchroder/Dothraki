ace4de79d7e9ec792851fe0d54cba6f30b1ea43a
Gustavo Lazarotto Schroeder <guto_schroeder@hotmail.com>
2016-09-20 15:11:30 -0300

    Agora adicionando a codificação

--- DothrakiServer/src/Server/Client.java
+++ DothrakiServer/src/Server/Client.java
@@ -1,5 +1,37 @@
 package Server;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+	public static void main(String argv[]) throws Exception {
+
+		String palavra = "";
+		String palavraModificada;
+
+		Scanner doUsuario = new Scanner(System.in);
+
+		Socket socketCliente = new Socket("localhost", 6789);
+		DataOutputStream paraServidor = new DataOutputStream(socketCliente.getOutputStream());
+		BufferedReader doServidor = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+
+		while (true) {
+			System.out.print("Digite uma palavra: ");
+			while (true) {
+				char x = doUsuario.next().charAt(0);
+				if (x == ' ') {
+					paraServidor.writeBytes(palavra);
+					palavraModificada = " " + doServidor.readLine();
+				} else if (x == '0') {
+					socketCliente.close();
+					return;
+				} else {
+					palavra += x;
+				}
+			}
+		}
+	}
 }
