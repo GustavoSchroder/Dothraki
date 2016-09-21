fcd95af7f84575bf3c9dc95621c13f11bba90ccb
Augusto Pasini <tuto_pasini@hotmail.com>
2016-09-20 21:56:03 -0300

    Update Server.java

--- DothrakiServer/src/Server/Server.java
+++ DothrakiServer/src/Server/Server.java
@@ -1,15 +1,27 @@
 package Server;
 
-public class Server {
-	
-	public static void main(String[] args) {
-		Dictionary dictionary = new Dictionary();
-		
-		String sentence = "I love you";
-		String result = dictionary.translate(sentence);
-		
-		System.out.println("English: " + sentence);
-		System.out.println("Dothraki: " + result);
-	}	
-}
+import java.io.*;
+import java.net.*;
 
+public class Server {
+
+	public static void main(String[] args) throws Exception {
+
+		Dictionary dictionary = new Dictionary();
+		ServerSocket receptionSocket = new ServerSocket(6789);
+		
+		while (true) {
+			
+			Socket connectionSocket = receptionSocket.accept();
+			BufferedReader fromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream toClient = new DataOutputStream(connectionSocket.getOutputStream());
+			String sentence = fromClient.readLine();
+
+			String result = dictionary.translate(sentence);
+			toClient.writeBytes(result);
+
+			System.out.println("English: " + sentence);
+			System.out.println("Dothraki: " + result);
+		}
+	}
+}
