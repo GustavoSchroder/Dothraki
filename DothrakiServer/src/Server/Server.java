834c727b3d15ffccb2ec8a6dbb9b9bd9b77957e9
Gustavo Lazarotto Schroeder <guto_schroeder@hotmail.com>
2016-10-05 21:36:50 -0300

    Agora aceita palavra sem to

--- DothrakiServer/src/Server/Server.java
+++ DothrakiServer/src/Server/Server.java
@@ -9,16 +9,35 @@ public class Server {
 
 		Dictionary dictionary = new Dictionary();
 		ServerSocket receptionSocket = new ServerSocket(6789);
-		
+
 		while (true) {
 			Socket connectionSocket = receptionSocket.accept();
 			BufferedReader fromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			DataOutputStream toClient = new DataOutputStream(connectionSocket.getOutputStream());
 			String sentence = fromClient.readLine();
-			String result = dictionary.translate(sentence);
-			//prints dorthaki in console
-			System.out.print(" " + result);
-			toClient.writeBytes(result);
+			System.out.println(sentence);
+			String[] parts = sentence.split(" ");
+			System.out.println(parts.length);
+			String result = "";
+			String output = "";
+			for (int i = 0; i < parts.length; i++) {
+				result = dictionary.translate(parts[i]);
+				if (result.contains("[")) {
+					result = "to " + parts[i];
+					result = " " + dictionary.translate(result);
+					if (result.contains("[")) {
+						output += " [" + result.substring(5, result.length());
+					} else {
+						output += result;
+					}
+				} else {
+					output += " " + result;
+				}
+			}
+
+			// prints dorthaki in console
+			 System.out.print(" " + result);
+			toClient.writeBytes(output);
 			connectionSocket.close();
 		}
 	}
